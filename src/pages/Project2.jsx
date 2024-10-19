3051924e11afe3af1dc0b6eb00a35cb2a002aea8
LaGodxy <83363896+LaGodxy@users.noreply.github.com>
2024-10-19 20:24:33 +0100

    update

--- src/pages/Project2.jsx
+++ src/pages/Project2.jsx
@@ -25,7 +25,7 @@ const Project = () => {
             <img
               src={searchNormal}
               alt="Search Icon"
-              className="absolute left-3 top-1/2 transform -translate-y-1/2 w-4 h-4"
+              className="absolute left-3 top-1/3 transform -translate-y-1/2 w-4 h-4"
             />
           </div>
           </div>
